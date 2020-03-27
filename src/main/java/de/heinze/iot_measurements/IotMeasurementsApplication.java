375c66c4dc101fce1c84eeac89facc5de40fa9d1
Christian Heinz <kontakt@christian-heinz.eu>
2020-03-27 20:32:12 +0100

    Initial setup

---
+++ src/main/java/de/heinze/iot_measurements/IotMeasurementsApplication.java
@@ -0,0 +1,13 @@
+package de.heinze.iot_measurements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMeasurementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotMeasurementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/heinze/iot_measurements/IotMeasurementsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heinze.iot_measurements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotMeasurementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
